6b9506b3d2246d4890fade6d202f2e65c6a27afe
rnh027 <naryne027@gmail.com>
2020-06-30 23:32:05 +0530

    changes

--- counting hrs.py
+++ counting hrs.py
@@ -5,7 +5,7 @@ count = list()
 for line in handle:
     if line.startswith("From") and ":" in line:
         i = line.find(':')
-        count.append(int(line[i-1]))
+        count.append(int(line[i-2:i]))
 tot = dict()
 for h in count:
     tot[h] = tot.get(h,0)+1
