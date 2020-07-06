3835da57a9b44d9b33a91b541470c305db7a3e52
rnh027 <naryne027@gmail.com>
2020-07-06 22:46:36 +0530

    ex1.c3

--- week3ex1.py
+++ week3ex1.py
@@ -1,4 +1,10 @@
-file = open(hello.txt)
-file1 = read(file)
-for line in file1:
-    print(line)
+import re
+
+file = open("original_data.txt")
+total = 0
+for line in file:
+    #extract number from each line and converted into list of integer
+    number = list(map(int,re.findall('[0-9]+',line)))
+    #store sum value of each line and add to total
+    total += sum(number)
+print(total)
